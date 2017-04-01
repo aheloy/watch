65819a0d4dfa6e53f34cf57b0a91fd1e97e9c16e
Admin <qwsaaswq321123@gmail.com>
2017-04-01 16:18:50 +0500

    small changes

--- app/watch.js
+++ app/watch.js
@@ -1,77 +1,77 @@
 
-;(function(){
+;(function () {
 
-	const DEFAULT_TIMEZONE = 'Europe/London';
+  const DEFAULT_TIMEZONE = 'Europe/London';
 
-	function Watch(element, timezone=DEFAULT_TIMEZONE) {
-		this._timezone = timezone;
+  function Watch(element, timezone = DEFAULT_TIMEZONE) {
+    this._timezone = timezone;
 
-		this._createWrappers(element);
-		this._createArrows();
-		this._createLabels();
+    this._createWrappers(element);
+    this._createArrows();
+    this._createLabels();
 
-		this._runWatch();
-	}
+    this._runWatch();
+  }
 
-	window.Watch = Watch;
+  window.Watch = Watch;
 
-	Watch.prototype._createWrappers = function(element) {
-		this._watch 								= createDiv(element, 'watch_default');
-		this._lineLabelsWrapper 		= createDiv(this._watch, 'labels');
-		this._boldLineLabelsWrapper = createDiv(this._watch, 'bold-labels');
-		this._numericLabelsWrapper 	= createDiv(this._watch, 'numeric-labels');
-		this._arrowsWrapper 				= createDiv(this._watch, 'arrows');
-	};
+  Watch.prototype._createWrappers = function (element) {
+    this._watch = createDiv(element, 'watch_default');
+    this._lineLabelsWrapper = createDiv(this._watch, 'labels');
+    this._boldLineLabelsWrapper = createDiv(this._watch, 'bold-labels');
+    this._numericLabelsWrapper = createDiv(this._watch, 'numeric-labels');
+    this._arrowsWrapper = createDiv(this._watch, 'arrows');
+  };
 
-	Watch.prototype._createArrows = function() {
-		this._hourArrow 	= createDiv(this._arrowsWrapper, 'watch__hour-arrow');
-		this._minuteArrow = createDiv(this._arrowsWrapper, 'watch__minute-arrow');
-		this._secondArrow = createDiv(this._arrowsWrapper, 'watch__second-arrow');
-	};
+  Watch.prototype._createArrows = function () {
+    this._hourArrow = createDiv(this._arrowsWrapper, 'watch__hour-arrow');
+    this._minuteArrow = createDiv(this._arrowsWrapper, 'watch__minute-arrow');
+    this._secondArrow = createDiv(this._arrowsWrapper, 'watch__second-arrow');
+  };
 
-	Watch.prototype._createLabels = function() {
-		createSeveralDivs(this._lineLabelsWrapper, 'watch__line-label', 60);
-		createSeveralDivs(this._boldLineLabelsWrapper, 'watch__line-label_bold', 12);
-		createSeveralDivs(this._numericLabelsWrapper, 'watch__numeric-label', 12);
-	};
+  Watch.prototype._createLabels = function () {
+    createSeveralDivs(this._lineLabelsWrapper, 'watch__line-label', 60);
+    createSeveralDivs(this._boldLineLabelsWrapper, 'watch__line-label_bold', 12);
+    createSeveralDivs(this._numericLabelsWrapper, 'watch__numeric-label', 12);
+  };
 
-	Watch.prototype._runWatch = function() {
-		setInterval( function() {
+  Watch.prototype._runWatch = function () {
+    setInterval(function () {
 
-			const datetime = moment.tz(moment().format(), this._timezone);
+      const datetime = moment.tz(moment().format(), this._timezone);
 
-			let hours = datetime.hours(); // 0...23
-			hours = hours > 12 ? hours - 12 : hours; // 0...12
-			let minutes = datetime.minute(); // 0...59
-			let seconds = datetime.second(); // 0...59
+      let hours = datetime.hours(); // 0...23
+      hours = hours > 12 ? hours - 12 : hours; // 0...12
+      let minutes = datetime.minute(); // 0...59
+      let seconds = datetime.second(); // 0...59
 
-			let rotateHourArrow = hours * 30 + minutes * 0.5 + seconds * 0.0083;
-			let rotateMinuteArrow = minutes * 6 + seconds * 0.1;
-			let rotateSecondArrow = seconds * 6;
+      let rotateHourArrow = hours * 30 + minutes * 0.5 + seconds * 0.0083;
+      let rotateMinuteArrow = minutes * 6 + seconds * 0.1;
+      let rotateSecondArrow = seconds * 6;
 
-			this._hourArrow.style.transform = `rotate(${rotateHourArrow}deg)`;
-			this._minuteArrow.style.transform = `rotate(${rotateMinuteArrow}deg)`;
+      this._hourArrow.style.transform = `rotate(${rotateHourArrow}deg)`;
+      this._minuteArrow.style.transform = `rotate(${rotateMinuteArrow}deg)`;
 
-			TweenLite.to(this._secondArrow, 0.3, { 
-				ease: Elastic.easeOut.config(1.2, 0.75),
-				rotation: rotateSecondArrow
-			});
+      TweenLite.to(this._secondArrow, 0.3, {
+        ease: Elastic.easeOut.config(1.2, 0.75),
+        rotation: rotateSecondArrow
+      });
 
-		}.bind(this), 1000);
-	};
-
-
+    }.bind(this), 1000);
+  };
 
 
-	function createDiv(parent, className) {
-		let elem = document.createElement('div');
-		elem.className = className;
-		return parent.appendChild(elem);
-	}
 
-	function createSeveralDivs(parent, className, number) {
-		let content = ('<div class="' + className + '"></div>').repeat(number);
-		parent.insertAdjacentHTML('beforeEnd', content);
-	}
+
+  function createDiv(parent, className) {
+    let elem = document.createElement('div');
+    elem.className = className;
+    return parent.appendChild(elem);
+  }
+
+  function createSeveralDivs(parent, className, number) {
+    let content = ('<div class="' + className + '"></div>').repeat(number);
+    parent.insertAdjacentHTML('beforeEnd', content);
+  }
 
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,115 @@
-var gulp 						= require('gulp');
-var sass 						= require('gulp-sass');
-var browserSync 		= require('browser-sync');
-var concat 					= require('gulp-concat');
-var concatCss 			= require('gulp-concat-css');
-var uglify 					= require('gulp-uglifyjs');
-var cssnano 				= require('gulp-cssnano');
-var rename 					= require('gulp-rename');
-var del 						= require('del');
-var autoprefixer 		= require('gulp-autoprefixer');
-var babel 					= require('gulp-babel');
-var sourcemaps 			= require("gulp-sourcemaps");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var uglify = require('gulp-uglifyjs');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var del = require('del');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var sourcemaps = require("gulp-sourcemaps");
+var replace = require('gulp-replace');
 
 
-
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: './',
-            index: 'demo/index.html'
-        },
-        notify: true
-    });
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: './',
+      index: 'demo/dev_index.html'
+    },
+    notify: true
+  });
 });
 
 
 
-gulp.task('sass-default', function() {
-	return gulp.src('app/themes/default/main.sass')
-	.pipe( sass() )
-	.pipe( autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }) )
-	.pipe( cssnano() )
-	.pipe( rename({
-		basename: 'default',
-		suffix: '.min',
-	}) )
-	.pipe( gulp.dest('app/themes/default') );
+gulp.task('sass-default', function () {
+  return gulp.src('app/themes/default/main.sass')
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(cssnano())
+    .pipe(rename({
+      basename: 'default',
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest('app/themes/default'));
 });
 
 
 
-gulp.task('sass-demo', function() {
-	return gulp.src('demo/demo.sass')
-	.pipe( sass() )
-	.pipe( autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }) )
-	.pipe( cssnano() )
-	.pipe( rename({
-		basename: 'demo',
-		suffix: '.min',
-	}) )
-	.pipe( gulp.dest('demo') )
-	.pipe( browserSync.reload({stream: true}) );
+gulp.task('sass-demo', function () {
+  return gulp.src('demo/demo.sass')
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(cssnano())
+    .pipe(rename({
+      basename: 'demo',
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest('demo'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 
 
-gulp.task('css', function() {
-	return gulp.src('app/themes/**/*.min.css')
-	.pipe( concatCss('dist/watch.min.css') )
-	.pipe( cssnano() )
-	.pipe( gulp.dest('./') )
-	.pipe( browserSync.reload({stream: true}) );
+gulp.task('css', function () {
+  return gulp.src('app/themes/**/*.min.css')
+    .pipe(concatCss('dist/watch.min.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 
 
-gulp.task('sass', ['sass-default'], function() {});
+gulp.task('sass', ['sass-default'], function () { });
 
 
 
-gulp.task('js', function() {
-	return gulp.src('app/*.js')
-	.pipe( sourcemaps.init() )
-	.pipe( babel({
-		'presets': ['env']
-	}) )
-	.pipe( concat('watch.min.js') )
-	.pipe( uglify() )
-	.pipe( sourcemaps.write('.') )
-	.pipe( gulp.dest('dist') )
-	.pipe( browserSync.reload({stream: true}) );
+gulp.task('js', function () {
+  return gulp.src('app/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      'presets': ['env']
+    }))
+    .pipe(concat('watch.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+
+
+gulp.task('html', function(){
+  return gulp.src('demo/dev_index.html')
+  .pipe(replace('/dist/', '/watch/dist/'))
+  .pipe(replace('/demo/', '/watch/demo/'))
+  .pipe(rename({
+      basename: 'index'
+    }))
+  .pipe(gulp.dest('demo'));
 });
 
 
 
 gulp.task('watch', [
-	'browser-sync', 
-	'sass',
-	'js',
-	], function() {
-		gulp.watch('demo/index.html', browserSync.reload);
-		gulp.watch('app/**/*.sass', ['sass']);
-		gulp.watch('app/**/*.css', ['css']);
-		gulp.watch('app/*.js', ['js']);
-		gulp.watch('demo/demo.sass', ['sass-demo']);
+  'browser-sync',
+  'html',
+  'sass',
+  'js',
+], function () {
+  gulp.watch('demo/dev_index.html', browserSync.reload);
+  gulp.watch('demo/dev_index.html', ['html']);
+  gulp.watch('app/**/*.sass', ['sass']);
+  gulp.watch('app/**/*.css', ['css']);
+  gulp.watch('app/*.js', ['js']);
+  gulp.watch('demo/demo.sass', ['sass-demo']);
 });
 
 
 
-gulp.task('default', ['watch'], function() {});
+gulp.task('default', ['watch'], function () { });
 
